a3a6b044e45c28d2849b6fdbb5e72bb61b3a163d
swamiabhishek45 <abhishekswami1435@gmail.com>
2026-07-15 17:38:36 +0530

    feat: initialize Prisma user schema and implement automated authentication onboarding logic

--- features/auth/action/onboard.ts
+++ features/auth/action/onboard.ts
@@ -1,9 +1,36 @@
-"use server";
-
+"use server"; 
 
 import { currentUser } from "@clerk/nextjs/server";
 
 import { prisma } from "@/lib/db";
 
-import type { User } from "@/lib/generated/prisma/client";
 
+export async function onBoard() {
+
+    const clerkUser = await currentUser();
+
+    if(!clerkUser){
+        throw new Error("Unauthorized")
+    }
+3
+    const email = clerkUser.emailAddresses[0]?.emailAddress ?? null;
+
+    // if data is alreday exist, update data
+    // if data is not present, create new data
+    return prisma.user.upsert({
+        where:{clerkId: clerkUser.id},
+        create: {
+            clerkId: clerkUser.id,
+            email,
+            firstName: clerkUser.firstName,
+            lastName: clerkUser.lastName,
+            imageUrl: clerkUser.imageUrl
+        },
+        update: {
+            email,
+            firstName: clerkUser.firstName,
+            lastName: clerkUser.lastName,
+            imageUrl: clerkUser.imageUrl
+        }
+    })
+}
